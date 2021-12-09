c56418ce87659a98ed8de98c4e42dbae36afb5c6
Luyang Liu <https://github.com/luyangliuable>
2021-12-09 12:15:08 +1100

    CSV file now able to read csv files
    	new file:   src/myfile.csv
    	modified:   src/read_csv_file.c

--- src/read_csv_file.c
+++ src/read_csv_file.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <math.h>
 
-int main(void) {
-  printf("Read csv file\n");
+#define MAXROWS 10
+#define MAXCOLS 5
+
+
+int main()
+{
+  int rowIndex = 0;
+  double rows[MAXROWS][MAXCOLS] = {{0,0}};
+  char line[128];
+  char* token = NULL;
+  FILE* fp = fopen("myfile.csv","r");
+  if (fp != NULL)
+    {
+      while (fgets( line, sizeof(line), fp) != NULL && rowIndex < MAXROWS)
+        {
+          int colIndex = 0;
+          for (token = strtok( line, ";"); token != NULL && colIndex < MAXCOLS; token = strtok(NULL, ";"))
+            {
+              rows[rowIndex][colIndex++] = atof(token);
+            }
+          rowIndex++;
+        }
+      fclose(fp);
+    }
+
+  for (int i = 0; i < rowIndex; ++i)
+    {
+      for (int j = 0; j < MAXCOLS; ++j)
+        printf("%10.4lf", rows[i][j]);
+      putchar('\n');
+    }
+
   return 0;
 }
-
